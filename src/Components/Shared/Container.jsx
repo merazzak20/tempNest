c3caa58a1b9a463d439a86769abf4a34cae72803
merazzak <info.merazzak@gmail.com>
2025-05-09 21:50:55 +0600

    first commit

---
+++ src/Components/Shared/Container.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Container = ({ children }) => {
+  return (
+    <div className="max-w-[2520 px] mx-auto xl:px-20 md:px-14 sm:px-10 px-6">
+      {children}
+    </div>
+  );
+};
+
+export default Container;
---
+++ src/Components/Shared/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "./Container";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Container>
+        <p>hello</p>
+      </Container>
+    </div>
+  );
+};
+
+export default Navbar;
